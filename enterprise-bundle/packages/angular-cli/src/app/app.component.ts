330c7ae1744627e0eaba0bea7a523d71ab2cca14
Stephen Cooper <scooperdev@gmail.com>
2024-12-02 16:51:28 +0000

    Fix Angular Grid Charts Bundle

--- enterprise-bundle/packages/angular-cli/src/app/app.component.ts
+++ enterprise-bundle/packages/angular-cli/src/app/app.component.ts
@@ -1,31 +1,49 @@
-import {Component, ViewEncapsulation} from '@angular/core';
-import {AgGridAngular} from 'ag-grid-angular';
-import {ColDef} from 'ag-grid-community';
+import { Component } from '@angular/core';
+import { AgGridAngular } from 'ag-grid-angular';
+import { ColDef, themeQuartz } from 'ag-grid-community';
 
-import { AllEnterpriseModule, LicenseManager, ModuleRegistry } from "ag-grid-enterprise";
-import { AgChartsEnterpriseModule } from "ag-charts-enterprise";
-ModuleRegistry.registerModules([AllEnterpriseModule.with(AgChartsEnterpriseModule)]);
-LicenseManager.setLicenseKey("<your license key>")
+import {
+  AllEnterpriseModule,
+  LicenseManager,
+  ModuleRegistry,
+} from 'ag-grid-enterprise';
+import { AgChartsEnterpriseModule } from 'ag-charts-enterprise';
+ModuleRegistry.registerModules([
+  AllEnterpriseModule.with(AgChartsEnterpriseModule),
+]);
+LicenseManager.setLicenseKey('<your license key>');
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [AgGridAngular],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
-  encapsulation: ViewEncapsulation.None
+  template: /*html*/ ` <ag-grid-angular
+    style="height: 100%;"
+    [theme]="theme"
+    [rowData]="rowData"
+    [columnDefs]="colDefs"
+    [defaultColDef]="defaultColDef"
+    [statusBar]="statusBar"
+    [enableCharts]="enableCharts"
+    [cellSelection]="cellSelection"
+    loadThemeGoogleFonts
+  >
+  </ag-grid-angular>`,
 })
 export class AppComponent {
+  theme = themeQuartz;
+  enableCharts = true;
+  cellSelection = true;
   rowData = [
-    {make: "Toyota", model: "Celica", price: 35000},
-    {make: "Ford", model: "Mondeo", price: 32000},
-    {make: "Porsche", model: "Boxster", price: 72000}
+    { make: 'Toyota', model: 'Celica', price: 35000 },
+    { make: 'Ford', model: 'Mondeo', price: 32000 },
+    { make: 'Porsche', model: 'Boxster', price: 72000 },
   ];
 
-  colDefs : ColDef[] = [
-    {field: "make"},
-    {field: "model"},
-    {field: "price"}
+  colDefs: ColDef[] = [
+    { field: 'make' },
+    { field: 'model' },
+    { field: 'price' },
   ];
 
   defaultColDef = {
@@ -37,7 +55,7 @@ export class AppComponent {
       {
         statusPanel: 'agTotalAndFilteredRowCountComponent',
         align: 'left',
-      }
-    ]
+      },
+    ],
   };
 }
