2aa2c80e2aa7c343f229e237d586c9afa8e74d9d
seanlandsman <sean@thelandsmans.com>
2025-06-11 16:27:44 +0100

    Remove old bundle seeds

--- enterprise/packages/create-next-app/next.config.ts
+++ enterprise/packages/create-next-app/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    basePath: '/seed-projects/enterprise/packages/create-next-app'
 };
 
 export default nextConfig;
